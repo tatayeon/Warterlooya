c01901180c5f822cf098aa55c894241fbb2cac47
taetae <dlaxodus1212@naver.com>
2024-11-23 22:47:50 +0900

    Feat: 환경설정

---
+++ src/main/java/com/example/Warterlooya/WarterlooyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Warterlooya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarterlooyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarterlooyaApplication.class, args);
+	}
+
+}
